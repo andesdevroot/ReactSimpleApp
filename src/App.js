12629835937aaa942d98392c87a6434536276d99
andesdevroot <andes.dev.root@gmail.com>
2024-12-25 19:01:33 -0300

    segundo commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// filepath: /home/cesar/Escritorio/ReactJS/curso/src/App.js
+import React from 'react';
+import './App.css';
+import HelloWorld from './Components/HelloWorld.js';
+
+function App() {
+  return (
+    <div>
+      <HelloWorld  name="César"/>
+    </div>
+  );
+}
+
+export default App;
+
+
